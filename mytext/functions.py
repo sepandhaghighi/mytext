b47d9b1cf9873554725ffe55f834d92ac82f4d6b
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-11-18 01:50:08 +0330

    feat : call_cloudflare function added

--- mytext/functions.py
+++ mytext/functions.py
@@ -73,6 +73,60 @@ def call_ai_studio(
         "model": selected_model}
 
 
+def call_cloudflare(
+        prompt: Prompt,
+        account_id: str,
+        api_key: str,
+        main_model="llama-3-8b-instruct",
+        fallback_model="llama-3-8b-instruct",
+        timeout=15,
+        max_retries=3,
+        retry_delay=0.5,
+        backoff_factor=1):
+    data = dict()
+    data["messages"] = [prompt.render(RenderFormat.OPENAI)]
+    retry_index = 0
+    error_message = ""
+    next_delay = retry_delay
+    selected_model = main_model
+    headers = {
+    "Authorization": "Bearer {api_key}".format(api_key=api_key),
+    "Content-Type": "application/json"
+}
+    while retry_index < max_retries:
+        try:
+            api_url = "https://api.cloudflare.com/client/v4/accounts/{account_id}/ai/run/@cf/meta/{model}".format(
+                account_id=account_id,
+                model=selected_model)
+            with requests.Session() as session:
+                response = session.post(
+                    api_url,
+                    headers=headers,
+                    json=data,
+                    timeout=timeout)
+                if response.status_code in (200, 201):
+                    response_data = response.json()
+                    return {
+                        "status": True,
+                        "message": response_data["result"]["response"],
+                        "model": selected_model}
+                elif response.status_code == 503:
+                    selected_model = fallback_model
+                raise Exception(
+                    "Status Code: {status_code}\n\nContent:\n{content}".format(
+                        status_code=response.status_code,
+                        content=response.text))
+        except Exception as e:
+            error_message = str(e)
+            retry_index += 1
+            time.sleep(next_delay)
+            next_delay = next_delay * backoff_factor
+    return {
+        "status": False,
+        "message": error_message,
+        "model": selected_model}
+
+
 def run_mytext(text: str, auth: dict, mode: Mode = Mode.PARAPHRASE, tone: Tone = Tone.NEUTRAL, provider: Provider = Provider.AI_STUDIO):
     instruction_str = build_instruction(mode, tone)
     template = PromptTemplate(
