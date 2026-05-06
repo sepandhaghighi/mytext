46eceaba033880feaaf0dd7e32d3719def2f750d
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-05-06 14:22:37 +0330

    feat : add errors.py

---
+++ mytext/errors.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""mytext errors."""
+
+class MyTextError(Exception):
+    """Base exception for all MyText errors."""
+
+
+class MyTextValidationError(MyTextError):
+    """Raised when input validation fails."""
+
+
+class MyTextProviderError(MyTextError):
+    """Raised when a provider call fails."""
