e2815d59f130ee9574c964c13d1d2bbb82e83c17
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-05-06 14:40:59 +0330

    fix : add error classes to __init__.py

--- mytext/__init__.py
+++ mytext/__init__.py
@@ -2,7 +2,8 @@
 """mytext modules."""
 from mytext.params import MY_TEXT_VERSION
 from mytext.params import Provider, Mode, Tone
+from mytext.errors import MyTextError, MyTextProviderError, MyTextValidationError
 from mytext.functions import run_mytext
 __version__ = MY_TEXT_VERSION
 
-__all__ = ["Provider", "Mode", "Tone", "run_mytext"]
+__all__ = ["Provider", "Mode", "Tone", "run_mytext", "MyTextError", "MyTextProviderError", "MyTextValidationError"]
